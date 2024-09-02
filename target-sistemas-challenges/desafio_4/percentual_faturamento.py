6cae46242eafb023b7b95a6d110f26e3c1a3c1ae
Vitor Vieira <b_.5vitor@hotmail.com>
2024-09-02 20:28:02 -0300

    desafios do estágio Target Sistemas

--- target-sistemas-challenges/desafio_4/percentual_faturamento.py
+++ target-sistemas-challenges/desafio_4/percentual_faturamento.py
@@ -1,16 +1,28 @@
-# percentual_faturamento.py
-import json
+# Dados de faturamento por estado
+faturamento_por_estado = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+}
 
-def percentual_faturamento(dados):
-    total = sum(dados)
-    percentuais = [(x / total) * 100 for x in dados]
+def calcular_percentuais(faturamento_por_estado):
+    faturamento_total = sum(faturamento_por_estado.values())
+    percentuais = {estado: (valor / faturamento_total) * 100 for estado, valor in faturamento_por_estado.items()}
     return percentuais
 
-if __name__ == "__main__":
-    with open('faturamento.json', 'r') as f:
-        faturamento = json.load(f)
-    
-    percentuais = percentual_faturamento(faturamento)
-    print("Percentuais de faturamento:")
-    for percentual in percentuais:
-        print(f"{percentual:.2f}%")
+def encontrar_maior_menor_estado(faturamento_por_estado):
+    estado_maior_faturamento = max(faturamento_por_estado, key=faturamento_por_estado.get)
+    estado_menor_faturamento = min(faturamento_por_estado, key=faturamento_por_estado.get)
+    return estado_maior_faturamento, estado_menor_faturamento
+
+percentuais = calcular_percentuais(faturamento_por_estado)
+estado_maior_faturamento, estado_menor_faturamento = encontrar_maior_menor_estado(faturamento_por_estado)
+
+print("Percentual de faturamento por estado:")
+for estado, percentual in percentuais.items():
+    print(f"{estado}: {percentual:.2f}%")
+
+print(f"\nEstado com o maior faturamento: {estado_maior_faturamento} - R${faturamento_por_estado[estado_maior_faturamento]:,.2f}")
+print(f"Estado com o menor faturamento: {estado_menor_faturamento} - R${faturamento_por_estado[estado_menor_faturamento]:,.2f}")
